8ce77f16bc7cdd18fd914867daafc9f40fff4e28
mc <mc@mc-2.local>
2013-06-30 19:01:06 -0400

    Initial commit

---
+++ CircularProgress/CircularProgress/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CircularProgress
+//
+//  Created by mc on 6/30/13.
+//  Copyright (c) 2013 mauricio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CircularProgress/CircularProgress/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CircularProgress
+//
+//  Created by mc on 6/30/13.
+//  Copyright (c) 2013 mauricio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
